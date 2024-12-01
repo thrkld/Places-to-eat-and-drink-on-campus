222f741b69d264da8a3f327f0dcba9bca2ac78d8
James Thirkeld <jamesthirkeld12345@gmail.com>
2024-12-01 16:19:44 +0000

    Create basic UI

--- Places to eat & Drink on Campus/ViewController.swift
+++ Places to eat & Drink on Campus/ViewController.swift
@@ -7,7 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        var content = UIListContentConfiguration.cell()
+        //content.text = "Place \(indexPath.row)"
+        /*
+        if places[indexPath.row]["name"] != nil {
+            content.text = places[indexPath.row]["name"]
+        }
+         */
+        cell.contentConfiguration = content
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
