97ffe2d728cebf89907699c0ed926f36d8f15b36
James Thirkeld <jamesthirkeld12345@gmail.com>
2024-12-01 17:36:40 +0000

    Slight reformatting

--- Places to eat & Drink on Campus/ViewController.swift
+++ Places to eat & Drink on Campus/ViewController.swift
@@ -28,23 +28,29 @@ struct Venue_Info: Codable {
     let last_modified: String
 }
 
-//view controller class
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
 
+import UIKit
+import MapKit
+import CoreLocation
+
+//view controller class
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate,
+MKMapViewDelegate, CLLocationManagerDelegate {
+    
     // MARK: Map & Location related stuff
     @IBOutlet weak var myMap: MKMapView!
     var locationManager = CLLocationManager()
     var firstRun = true
     var startTrackingTheUser = false
-
-    func locationManager(
-        _ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let locationOfUser = locations[0] //this method returns an array of locations
         //generally we always want the first one (usually there's only 1 anyway)
         let latitude = locationOfUser.coordinate.latitude
         let longitude = locationOfUser.coordinate.longitude
         //get the users location (latitude & longitude)
         let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
         if firstRun {
             firstRun = false
             let latDelta: CLLocationDegrees = 0.0025
@@ -55,73 +61,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             let region = MKCoordinateRegion(center: location, span: span)
             //make the map show that region we just defined.
             self.myMap.setRegion(region, animated: true)
-            //the following code is to prevent a bug which affects the zooming of the map to the
-            //user's location.
-            //We have to leave a little time after our initial setting of the map's location and span,
-            //before we can start centering on the user's location, otherwise the map never zooms in
-            //because the initial zoom level and span are applied to the setCenter() method call,
-            //rather than our "requested" ones, once they have taken effect on the map.
+            //the following code is to prevent a bug which affects the zooming of the map to the user's location.
+            //We have to leave a little time after our initial setting of the map's location and span, before we can start centering on the user's location, otherwise the map never zooms in because the intial zoom level and span are applied to the setCenter( ) method call, rather than our "requested" ones,
+            //once they have taken effect on the map.
+            
             //we setup a timer to set our boolean to true in 5 seconds.
             _ = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector:
-                #selector(startUserTracking), userInfo: nil, repeats: false)
-        }
-        if startTrackingTheUser {
-            myMap.setCenter(location, animated: true)
-        }
+        #selector(startUserTracking), userInfo: nil, repeats: false)
+            }
+            if startTrackingTheUser == true {
+                myMap.setCenter(location, animated: true)
+            }
     }
-
+    
     //this method sets the startTrackingTheUser boolean class property to true. Once it's true,
     //subsequent calls to didUpdateLocations will cause the map to centre on the user's location.
     @objc func startUserTracking() {
         startTrackingTheUser = true
-        
     }
-
+    
     //MARK: Table related stuff
+    
     @IBOutlet weak var theTable: UITableView!
 
-    func tableView(
-        _ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-
-    func tableView(
-        _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        var content = UIListContentConfiguration.subtitleCell()
-        content.text = "testing"
-        content.secondaryText = "more testing"
-        cell.contentConfiguration = content
-        return cell
-    }
-
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 10
+        }
+    
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+            var content = UIListContentConfiguration.subtitleCell()
+            content.text = "\(indexPath.row)"
+            content.secondaryText = "aa"
+            cell.contentConfiguration = content
+            return cell
+        }
+    
     // MARK: View related Stuff
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //get json information
-        if let url = URL(string: "https://cgi.csc.liv.ac.uk/~phil/Teaching/COMP228/eating_venues/data.json") {
-                   let session = URLSession.shared
-                   session.dataTask(with: url) { (data, response, err) in
-                       guard let jsonData = data else {
-                           return
-                       }
-                       do {
-                           let decoder = JSONDecoder()
-                           let venuInfo = try decoder.decode(FoodData.self, from: jsonData)
-                           var count = 0
-                           for aVenue in venuInfo.food_venues {
-                               count += 1
-                               print("\(count) " + aVenue.name)
-                           }
-                       } catch let jsonErr {
-                           print("Error decoding JSON", jsonErr)
-                       }
-                   }.resume()
-                   print("You are here!")
-               }
-        
-        
         // Make this view controller a delegate of the Location Manager, so that it
         //is able to call functions provided in this view controller.
         locationManager.delegate = self as CLLocationManagerDelegate
@@ -137,5 +115,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         locationManager.startUpdatingLocation()
         //configure the map to show the user's location (with a blue dot).
         myMap.showsUserLocation = true
+        
+        //acquiring json information
+        if let url = URL(string: "https://cgi.csc.liv.ac.uk/~phil/Teaching/COMP228/eating_venues/data.json"){
+            let session = URLSession.shared
+            session.dataTask(with: url) { (data, response, err) in
+                guard let jsonData = data else {
+                    return
+                }
+                do {
+                    let decoder = JSONDecoder()
+                    let venuInfo = try decoder.decode(FoodData.self, from: jsonData)
+                    var count = 0
+                    for aVenue in venuInfo.food_venues {
+                        count += 1
+                        print("\(count) " + aVenue.name) }
+                } catch let jsonErr {
+                    print("Error decoding JSON", jsonErr)
+                }
+            }.resume()
+            print("You are here!")
+        }
     }
 }
