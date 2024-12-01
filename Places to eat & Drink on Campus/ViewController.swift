a12a9bd3ba3dd29188db743b58bc6139b7293195
James Thirkeld <jamesthirkeld12345@gmail.com>
2024-12-01 16:11:22 +0000

    Initial Commit

---
+++ Places to eat & Drink on Campus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places to eat & Drink on Campus
+//
+//  Created by James Thirkeld on 01/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
